6be9c3bc9300ea46d90b32dbbde9050635eb0161
Chris Tan <csytan@gmail.com>
2011-06-24 10:51:25 -0700

    Cache & show only the top topic from HN since the API is down all the time.

--- main.py
+++ main.py
@@ -3,46 +3,39 @@ import logging
 import os
 import wsgiref.handlers
 
-from google.appengine.api import memcache
+from google.appengine.ext import db
 from google.appengine.api import urlfetch
 
-import simplejson as json
+from django.utils import simplejson as json
 
 import tornado.wsgi
 import tornado.web
 
 
 
-class Topics(tornado.web.RequestHandler):
+class Cache(db.Model):
+    json = db.TextProperty()
+
+
+class Index(tornado.web.RequestHandler):
     def get(self):
-        topics = memcache.get('topics')
-        if topics is None:
-            try:
-                response = urlfetch.fetch('http://api.ihackernews.com/page', deadline=10)
-                data = json.loads(response.content)
-            except Exception, e:
-                logging.error(e)
-                return self.render('error.html')
-            topics = data['items']
-            memcache.add('topics', topics)
-        self.render('topics.html', topics=topics)
-
-
-class Topic(tornado.web.RequestHandler):
-    def get(self, id):
-        topic = memcache.get('topic:' + id)
-        if topic is None:
-            try:
-                topic = self.fetch_topic(id)
-            except Exception, e:
-                logging.error(e)
-                return self.render('error.html')
-            memcache.add('topic:' + id, topic)
-        self.render('topic.html', topic=topic)
-        
+        cache = Cache.get_by_key_name('cache')
+        if not cache or self.get_argument('update', None):
+            topic = self.fetch_topic()
+            cache = Cache(key_name='cache', json=json.dumps(topic))
+            cache.put()
+        else:
+            topic = json.loads(cache.json)
+        self.render('topic.html', topic=topic, graph=json.dumps(topic['graph']))
+    
     @staticmethod
-    def fetch_topic(id):
-        response = urlfetch.fetch('http://api.ihackernews.com/post/' + id, deadline=10)
+    def fetch_topic():
+        response = urlfetch.fetch('http://api.ihackernews.com/page', deadline=10)
+        topics = json.loads(response.content)
+        topic_id = str(topics['items'][0]['id'])
+        
+        logging.error(topic_id)
+        response = urlfetch.fetch('http://api.ihackernews.com/post/' + topic_id, deadline=10)
         topic = json.loads(response.content)
         topic['graph'] = {}
         topic['all_comments'] = []
@@ -70,8 +63,7 @@ settings = {
     'debug': os.environ['SERVER_SOFTWARE'].startswith('Dev')
 }
 application = tornado.wsgi.WSGIApplication([
-    (r'/', Topics),
-    (r'/(.+)', Topic)
+    (r'/', Index),
 ], **settings)
 
 
